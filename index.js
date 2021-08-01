5aa23c3b3b951a3b2eb2b802d297fb722fde9cbb
ABRAHAM-1998 <abrahamjolly3377@gmail.com>
2021-08-01 17:37:42 +0530

    cbvc bcb

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors())
 const Chat = require('./Routes/chat');
 app.use(require('./Routes/chat'));
 
-app.post("/Chat",chat.createChat);
+app.post("/Chat", chat.createChat);
 
 //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 //PORT
@@ -25,36 +25,37 @@ const server = app.listen(PORT, console.log('PORT RUNNING ON ::::::4202:::::'))
 
 //DATABASE CONNNECTION
 db.connect((err) => {
-    if (err) {
-      console.log('unable to connect to database');
-      process.exit(1);
-    }
-  
-    else {
-  
-      console.log('CONNECTED TO MONGO :::27017::::');
-    }
-  });
+  if (err) {
+    console.log('unable to connect to database');
+    process.exit(1);
+  }
+
+  else {
+
+    console.log('CONNECTED TO MONGO :::27017::::');
+  }
+});
 //===========================++++++++++++========++++++======+++++=
 
-  const io = socket(server);
-  var count = 0;
-  //Socket.io Connection------------------
+const io = socket(server);
+var count = 0;
+//Socket.io Connection------------------
 io.on('connection', (socket) => {
 
   console.log("New socket connection: " + socket.id)
 
   socket.on('counter', () => {
-      count++;
-      console.log(count)
-      io.emit('counter', count);
+    count++;
+    console.log(count)
+    io.emit('counter', count);
+  })
+
+  socket.on('typing', (data) => {
+    io.emit(data.channelid, data)
+    console.log(data)
   })
 })
 
-socket.on('typing', (data) => {
-  io.emit(data.channelid, data)
-  console.log(data)
-})
 //+++++++++++++++++++++=====================++++++++++++++++++==============
 
 module.exports = { server }
